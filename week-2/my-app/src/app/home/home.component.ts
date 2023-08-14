2b2774160bb4bea9512b019858950874e0663420
Tiffany Reyes <tiffanyreyes.tr@outlook.com>
2023-08-14 15:48:37 -0400

    adding exercise 2.2

---
+++ week-2/my-app/src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+/**
+ * Title: home.component.ts
+ * Author: Tiffany Reyes
+ * Date: 14 Aug 2023
+ * Description: home component
+ */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
